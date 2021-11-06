1aed5f276112719708f97eea8c0eb334bed405b7
VP <VP>
2021-11-06 09:16:26 +0200

    Initial Commit

---
+++ AnalogClock/AnalogClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnalogClockApp.swift
+//  AnalogClock
+//
+//  Created by valentine on 06.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct AnalogClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnalogClock/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnalogClock
+//
+//  Created by valentine on 06.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
